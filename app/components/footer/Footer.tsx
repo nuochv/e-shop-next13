fb95142b240dd4ecfa1e5c9683593146bbf03022
hvnuoc <108932131+hvnuoc@users.noreply.github.com>
2024-01-12 18:27:30 +0700

    first commit

---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import Container from "../Container";
+import FooterList from "./FooterList";
+
+const Footer = () => {
+  return (
+    <footer className="bg-slate-700 text-slate-200 text-sm mt-16">
+      <Container>
+        <div className="flex flex-col md:flex-row justify-between pt-16 pb-8">
+          <FooterList>
+            <h3 className="text-base font-bold mb-2">Shop Categories</h3>
+            <Link href="#">Phones</Link>
+            <Link href="#">Laptops</Link>
+            <Link href="#">Desktops</Link>
+            <Link href="#">Watches</Link>
+            <Link href="#">Tvs</Link>
+            <Link href="#">Accessories</Link>
+          </FooterList>
+          <FooterList>
+            <h3 className="text-base font-bold mb-2">Customer Service</h3>
+            <Link href="#">Contact Us</Link>
+            <Link href="#">Shipping Policy</Link>
+            <Link href="#">Return & Exchanges</Link>
+            <Link href="#">Watches</Link>
+            <Link href="#">FAQs</Link>
+          </FooterList>
+        </div>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
